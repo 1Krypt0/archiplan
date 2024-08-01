6066bccb68826344c8aa771f1cd4b14f316ab680
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2024-08-01 13:42:58 +0100

    Add logout option and way to validate login

---
+++ src/routes/clients/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { PageServerData } from './$types';
+
+	export let data: PageServerData;
+
+	const userId = data.userId;
+</script>
+
+<main>Hello, user with ID {userId}</main>
+
+<form action="/logout" method="post">
+	<button type="submit">Logout</button>
+</form>
