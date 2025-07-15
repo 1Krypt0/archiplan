db2c24d1f2dd2bcb4371cd91b346399d645c6e5d
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2025-07-15 16:40:53 +0100

    Add a template sidebar with charts and data tables for easy modification

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,6 +5,7 @@ import {
   createRootRoute,
   HeadContent,
   Scripts,
+  Link,
 } from "@tanstack/react-router";
 
 import appCss from "@/styles/app.css?url";
@@ -32,7 +33,38 @@ export const Route = createRootRoute({
     ],
   }),
   component: RootComponent,
+  notFoundComponent: NotFoundComponent,
 });
+
+function NotFoundComponent() {
+  return (
+    <main className="flex min-h-screen flex-col">
+      <div className="flex w-full items-center justify-center border-b">
+        <div className="flex w-full max-w-[1280px] items-center py-4 gap-x-8 px-4 md:px-8">
+          <div className="md:flex-0 min-w-fit flex-1">
+            <Link to="/" className="pointer flex items-center">
+              <img src="/logo.svg" alt="Logo" width={150} height={100} />
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="mx-auto my-20 flex h-full w-full max-w-[1280px] flex-1 justify-center">
+        <main className="flex h-full w-full flex-col items-center gap-24 px-8 pb-24 pt-11 text-center md:gap-36 md:px-32 md:py-36">
+          <section id="features" className="flex flex-col items-center gap-12">
+            <h1 className="max-w-2xl !text-5xl md:!text-7xl">404</h1>
+            <h5 className="max-w-2xl !text-xl md:!text-xl" variant="h5">
+              Sorry! We could not find this page. But don&apos;t worry, there is
+              plenty of other things to see. Go look at those!
+            </h5>
+            <Link to="/" className="underline">
+              Go Back
+            </Link>
+          </section>
+        </main>
+      </div>
+    </main>
+  );
+}
 
 function RootComponent() {
   return (
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,12 @@ import { createServerFn } from "@tanstack/react-start";
 import { auth } from "@/lib/auth";
 import { getWebRequest } from "@tanstack/react-start/server";
 import { authMiddleware } from "@/lib/middleware";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SiteHeader } from "@/components/site-header";
+import { SectionCards } from "@/components/section-cards";
+import { ChartAreaInteractive } from "@/components/chart-area-interactive";
+import { DataTable } from "@/components/data-table";
 
 export const getCurrentUser = createServerFn({ method: "GET" })
   .middleware([authMiddleware])
@@ -17,18 +23,32 @@ export const Route = createFileRoute("/")({
 });
 
 function Home() {
-  const session = Route.useLoaderData();
-
-  console.log("Session on Home is");
-  console.log(session?.user.name);
+  const data: any[] = [];
 
   return (
-    <>
-      <div>This will be the main dashboard eventually!</div>
-      <p>
-        The current user is {session?.user.name} with a session of{" "}
-        {session?.session.id}
-      </p>
-    </>
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <SectionCards />
+              <div className="px-4 lg:px-6">
+                <ChartAreaInteractive />
+              </div>
+              <DataTable data={data} />
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
